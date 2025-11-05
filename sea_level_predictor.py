b256147aa5d74d56fda18d7a99a849b3bc1adf5b
sukhjot <legendcrow212@gmail.com>
2025-11-05 19:35:51 +0530

    just some modifications

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -12,21 +12,21 @@ def draw_plot():
     plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
     #first line of best fit using linregress
-    line_A = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    x_A = np.arange(df['Year'].min(), 2051, 1)
-    y_A =  x_A*line_A.slope + line_A.intercept
+    line1 = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    x1 = np.arange(df['Year'].min(), 2051, 1)
+    y1 =  x1*line1.slope + line1.intercept
 
-    plt.plot(x_A, y_A)
+    plt.plot(x1, y1)
 
     #Plotting a new line of best fit just using the data from year 2000 through the most recent year in the dataset and Making the line also go through the year 2050 to predict the sea level rise in 2050 if the rate of rise continues as it has since the year 2000.
     df_new = df[df['Year'] >= 2000]
 
     #second line of best fit using linregress
-    line_B = linregress(df_new['Year'], df_new['CSIRO Adjusted Sea Level'])
-    x_B = np.arange(2000,2051,1)
-    y_B = x_B*line_B.slope + line_B.intercept
+    line2 = linregress(df_new['Year'], df_new['CSIRO Adjusted Sea Level'])
+    x2 = np.arange(2000,2051,1)
+    y2 = x2*line2.slope + line2.intercept
 
-    plt.plot(x_B, y_B)
+    plt.plot(x2, y2)
 
     plt.xlabel('Year')
     plt.ylabel('Sea Level (inches)')
